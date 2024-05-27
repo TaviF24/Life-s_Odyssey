f45b128fd40d1cfc1ab1d335a7e3a17851704706
AlexTrandafir09 <alexandru-ionut.trandafir@s.unibuc.ro>
2024-05-27 14:08:02 +0300

    crafting table

--- Code/Life's Odyssey/Assets/Scripts/Data/ItemContainer.cs
+++ Code/Life's Odyssey/Assets/Scripts/Data/ItemContainer.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 [Serializable]
@@ -88,4 +89,27 @@ public class ItemContainer : ScriptableObject
             }
         }
     }
+
+    internal bool CheckItem(ItemSlot checkingItem)
+    {
+        ItemSlot itemSlot=slots.Find(x => x.item == checkingItem.item);
+        if(itemSlot==null){return false;}
+
+        if(checkingItem.item.stackable){return itemSlot.count > checkingItem.count;}
+        return true;
+    }
+
+
+    internal bool FreeSpace()
+    {
+        for(int i=0;i<slots.Count;i++)
+            {
+                if(slots[i].item==null)
+                {
+                    return true;
+                }
+            }
+        return false;
+    }
+
 }

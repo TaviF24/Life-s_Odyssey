9ef00fab72d2a60b54de1fc16e708df5e69e3618
legian-andrei <andreilegian138@yahoo.com>
2024-06-12 10:40:07 +0300

    made crops transition compatible

--- Code/Life's Odyssey/Assets/Scripts/TileMapReadController.cs
+++ Code/Life's Odyssey/Assets/Scripts/TileMapReadController.cs
@@ -11,6 +11,13 @@ public class TileMapReadController : MonoBehaviour
 
     public Vector3Int GetGridPosition(Vector2 position, bool mousePosition)
     {
+        if (tilemap == null)
+        {
+            tilemap = GameObject.Find("BaseTilemap").GetComponent<Tilemap>();
+        }
+
+        if (tilemap == null) { return Vector3Int.zero; }
+        
         Vector3 worldPosition;
 
         if (mousePosition)
@@ -28,8 +35,15 @@ public class TileMapReadController : MonoBehaviour
 
     public TileBase GetTileBase(Vector3Int gridPosition)
     {
-        
-        TileBase tile = tilemap.GetTile(gridPosition);  // type of tile you are clicking
+
+		if (tilemap == null)
+		{
+			tilemap = GameObject.Find("BaseTilemap").GetComponent<Tilemap>();
+		}
+
+		if (tilemap == null) { return null; }
+
+		TileBase tile = tilemap.GetTile(gridPosition);  // type of tile you are clicking
 
         Debug.Log(gridPosition + " " + tile);
 
